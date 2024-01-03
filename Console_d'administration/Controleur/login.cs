7b42d08aa2981e28627f9a0041937b6d438738e1
loic-hpp <loic.nguemegne-temena@polymtl.ca>
2024-01-03 13:23:18 -0500

    handle bad access

--- Console_d'administration/Controleur/login.cs
+++ Console_d'administration/Controleur/login.cs
@@ -2,7 +2,16 @@ namespace Controleur
 {
     class Login
     {
+        public static bool aAcces(string nomUtilisateur, string motDePasse)
+        {
+            return identifiantValide(nomUtilisateur, motDePasse) && estAdmin(nomUtilisateur);
+        }
         public static bool identifiantValide(string nomUtilisateur, string motDePasse)
+        {
+            return true;
+        }
+
+        public static bool estAdmin(string nomUtilisateur)
         {
             return true;
         }
--- Console_d'administration/Vue/Login_page/Login_form.cs
+++ Console_d'administration/Vue/Login_page/Login_form.cs
@@ -19,15 +19,19 @@ namespace login_form
 
         private void boutonConnecter_Click(object sender, EventArgs e)
         {
-            bool aAcces = Controleur.Login.identifiantValide(this.champNomUtilisateur.Text, this.champMotDePasse.Text);
-            if (aAcces) {
+            bool aAcces = Controleur.Login.aAcces(this.champNomUtilisateur.Text, this.champMotDePasse.Text);
+            if (aAcces)
+            {
                 this.Hide();
                 Form accueilForm = new Console_d_administration.Vue.Accueil.Accueil();
-                accueilForm.Closed += (s, args) => this.Close();
+                accueilForm.Closed += (s, args) => Application.Exit();
                 accueilForm.Show();
-
-
             }
+            else if (!Controleur.Login.identifiantValide(this.champNomUtilisateur.Text, this.champMotDePasse.Text))
+                MessageBox.Show("Nom d'utilisateur ou mot de passe incorrect");
+            else
+                MessageBox.Show("Vous n'avez pas d'accès administrateur");
+
         }
     }
 }
